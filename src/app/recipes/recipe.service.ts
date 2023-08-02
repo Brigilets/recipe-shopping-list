d28600d911d1befc2c1a8d79466937fdf4aeb4f8
Brigilets <b.sabutyte@gmail.com>
2023-08-02 09:12:40 +0200

    added child routes in recipes component, displaying recipe-start if no recipe is selected and dynamic route to display recipe-detail component

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -41,6 +41,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(index: number): Recipe {
+    return this.recipes[index];
+  }
+
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addMultipleIngredients(ingredients);
   }

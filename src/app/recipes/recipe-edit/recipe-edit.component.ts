8c099de138e783045ee91486f14eb1abbc478990
Brigilets <b.sabutyte@gmail.com>
2023-08-02 10:10:27 +0200

    cleaned up the method in <app-header> and unused css files

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -5,7 +5,6 @@ import { ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit {
   id: number;

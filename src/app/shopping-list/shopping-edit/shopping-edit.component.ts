dec5f695023ea9129946212b44fb6693440d45bd
Brigilets <b.sabutyte@gmail.com>
2023-08-08 16:11:41 +0200

    delete item functionality, displaying delete button only if we're in edit mode(item is selected)

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -58,6 +58,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
   ngOnDestroy(): void {
     this.shoppingListSubscription.unsubscribe();
   }

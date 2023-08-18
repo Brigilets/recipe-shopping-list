626743fdd62b9914b064525e31559376c83159c3
Brigilets <b.sabutyte@gmail.com>
2023-08-18 13:53:34 +0200

    adding save existing recipes to firebase db service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,11 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
 
   fetchRecipes() {}
 
-  addRecipe() {}
+  addRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put(
+        'https://recipe-shopping-app-fafdb-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log('res from add recipes', response);
+      });
+  }
 }
